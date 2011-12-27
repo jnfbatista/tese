333514c1d59ae0071c9bedb94cb9970521c4a83f
João Batista <batista@batista-laptop.(none)>
2011-12-27 19:41:31 +0000

    configuracao do cmake com o libfreenect e com o pcl

---
+++ projecto/main.cpp
@@ -0,0 +1,17 @@
+#include <libfreenect.hpp>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char * argv[], char * envp[]) {
+
+	// The thread which gets the kinect feed will be here
+	
+	// the thread to process/recognize it will be here
+
+	printf("Hello world!\n");
+	return 0;
+}
---
+++ projecto/todo.md
@@ -0,0 +1,7 @@
+# Project TODO List
+
+1. Use PCL with libfreenect
+2. Apply operations on PCL
+3. Detect primary shapes
+4. Detect complex shapes
+
